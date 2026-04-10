09cb8e193ed6ebcac6d4e42d337a59b3e1dbeb3e
palaciosyost <palaciosyostin903@gmail.com>
2026-04-10 14:44:25 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Custom Valmore Myr Extends",
+    "version": "1.0",
+    "category": "Custom",
+    "summary": "Extensión personalizada para Valmore Myr",
+    "description": """
+        Este módulo extiende las funcionalidades de Valmore Myr para adaptarse a las necesidades específicas de la empresa.
+    """,
+    'author': 'MYR CONSULTORIA EN SISTEMAS S.A.C',
+    'website': 'https://myrconsulting.net/',
+    "depends": [
+        "base", "contacts"
+    ],
+    "data": [
+        "view/form_view_contact.xml"
+    ],
+}
---
+++ model/contacto.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    tamanio_empresa = fields.Selection([
+        ('gran_empresa', 'Gran Empresa'),
+        ('mediana_empresa', 'Mediana Empresa'),
+        ('pequena_empresa', 'Pequeña Empresa'),
+        ('microempresa', 'Microempresa'),
+        ('entidad_publica', 'Entidad Pública'),
+        ('banco_financiera', 'Banco / Financiera'),
+        ('ong', 'ONG'),
+    ], string='Tamaño de Empresa')
+
+    segmentacion = fields.Selection([
+        ('a_estrategico', 'A - Estratégico'),
+        ('b_recurrente', 'B - Recurrente'),
+        ('c_ocasional', 'C - Ocasional'),
+    ], string='Segmentación')
+    linea_negocio = fields.Selection([
+        ('INM', 'Inmuebles'),
+        ('MAQ', 'Maquinaria y Equipo'),
+        ('VEH', 'Vehículos'),
+        ('INV', 'Inventarios'),
+        ('MER', 'Merma'),
+        ('EMP', 'Valorización de empresas en marcha'),
+        ('ESP', 'Servicios especiales / Atípicos'),
+    ], string='Línea de Negocio')    
+    cliente_recurrente = fields.Boolean(string='Cliente recurrente')
